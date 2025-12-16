b638ac062da8a4b180459b92146ecd6ac2fb5a7d
MrProfessorr <mrprofessorr88@gmail.com>
2025-12-16 12:46:24 +0800

    Update firebase-config.js

--- online/js/firebase-config.js
+++ online/js/firebase-config.js
@@ -1,8 +1,5 @@
-// admin/js/firebase-config.js  dan  online/js/firebase-config.js
-
-// ⚠️ Semua value di bawah ambil dari Firebase console (Project Settings → Config)
 const firebaseConfig = {
-  apiKey: "AIzaSyDbqBSY2v_HnTkGrB3BJ0TakdpF_FjsriE",      // GANTI DENGAN punyamu
+  apiKey: "AIzaSyDbqBSY2v_HnTkGrB3BJ0TakdpF_FjsriE",
   authDomain: "site-b902d.firebaseapp.com",
   databaseURL: "https://site-b902d-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "site-b902d",
@@ -10,13 +7,8 @@ const firebaseConfig = {
   messagingSenderId: "130388557538",
   appId: "1:130388557538:web:23715b27aa571e7fbf28bb"
 };
-
-// Inisialisasi Firebase (versi compat)
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
-
-// Realtime Database reference global
 window.db = firebase.database();
-// Debug supaya kita tahu file ini benar-benar jalan
 console.log("firebase-config LOADED, db =", !!db);
