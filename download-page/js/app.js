482bc6dde18a7555199a3669c50c532517e68cb9
MrProfessorr <mrprofessorr88@gmail.com>
2026-01-04 13:06:39 +0800

    Update app.js

--- download-page/js/app.js
+++ download-page/js/app.js
@@ -1,4 +1,4 @@
-// ====== CONFIG (Kau cuma tukar bahagian ini) ======
+
 const CONFIG = {
   telegramUrl: "https://t.me/Kiwi5G88?text=Hi",whatsappUrl: "https://wa.me/601137099600?text=Hi",
 
@@ -13,7 +13,6 @@ const CONFIG = {
   }
 };
 
-// ====== SET UI TEXT ======
 document.getElementById("year").textContent = new Date().getFullYear();
 
 document.getElementById("btnTelegram").href = CONFIG.telegramUrl;
@@ -23,14 +22,12 @@ document.getElementById("androidSub").textContent = CONFIG.android.label;
 document.getElementById("iosSub").textContent = CONFIG.ios.label;
 document.getElementById("winSub").textContent = CONFIG.windows.label;
 
-// ====== SAFE DOWNLOAD HANDLER ======
 function goDownload(url, hintElId){
   if(!url || url.includes("PASTE_")){
     const hint = document.getElementById(hintElId);
-    hint.textContent = "❗ Link belum diisi. Sila paste link download dalam js/app.js (CONFIG).";
+    hint.textContent = "❗This link not allowed for this moment.";
     return;
   }
-  // direct open (download)
   window.location.href = url;
 }
 
@@ -38,7 +35,6 @@ document.getElementById("btnAndroid").addEventListener("click", ()=> goDownload(
 document.getElementById("btnIOS").addEventListener("click", ()=> goDownload(CONFIG.ios.url, "iosHint"));
 document.getElementById("btnWindows").addEventListener("click", ()=> goDownload(CONFIG.windows.url, "winHint"));
 
-// ====== OPTIONAL: Hide iOS button if empty ======
 if(!CONFIG.ios.url || CONFIG.ios.url.trim()===""){
   document.getElementById("btnIOS").disabled = true;
   document.getElementById("btnIOS").textContent = "iOS (Link not available)";
