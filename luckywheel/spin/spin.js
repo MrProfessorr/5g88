3da46ccd8a8b992a845012b7372bb3bd9e33fb8b
MrProfessorr <mrprofessorr88@gmail.com>
2026-01-20 21:21:05 +0800

    Update spin.js

--- luckywheel/spin/spin.js
+++ luckywheel/spin/spin.js
@@ -506,7 +506,7 @@ $("btnSpin").onclick = async ()=>{
   if(!currentPrize || !currentCode) return;
   await armSoundsOnce();
   playStart();
-  const SPIN_MS = 6500;
+  const SPIN_MS = 6300;
   startTickLoopFree(SPIN_MS);
   spinning = true;
   $("btnSpin").disabled = true;
