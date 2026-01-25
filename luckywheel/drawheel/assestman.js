cdff608b4c453c1d7090a5636d0c038b246a843e
MrProfessorr <mrprofessorr88@gmail.com>
2026-01-25 21:00:37 +0800

    Update assestman.js

--- luckywheel/drawheel/assestman.js
+++ luckywheel/drawheel/assestman.js
@@ -1,22 +1,10 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-  import {getDatabase, ref, set, get, child, onValue, query, limitToLast,remove,update} 
-  from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
-  import { getAuth, onAuthStateChanged, signOut } 
-  from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDSqpbnkb3GLNFlHLYSz5XyRYPvKLAOCOA",
-  authDomain: "lucky-spined.firebaseapp.com",
-  databaseURL: "https://lucky-spined-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "lucky-spined",
-  storageBucket: "lucky-spined.firebasestorage.app",
-  messagingSenderId: "708886212396",
-  appId: "1:708886212396:web:2cb7a900fe1a7891510689"
-};
-
-  const app = initializeApp(firebaseConfig);
-  const db = getDatabase(app);
-  const auth = getAuth(app);
+import { db, auth } from "../share/cmdconfig-firebase-jsq1a42b.js";
+import {
+  ref, set, get, child, onValue, query, limitToLast, remove, update
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import {
+  onAuthStateChanged, signOut
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 const LS_USERMENU = "admin.navUsername";
 function saveNavUsername(name){
   localStorage.setItem(LS_USERMENU, String(name || "").trim());
