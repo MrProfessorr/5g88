e806314cda9276dbed88cb03913dd692019a3528
MrProfessorr <mrprofessorr88@gmail.com>
2026-01-26 16:39:25 +0800

    Update jsimpactsiteautoload4a3b21aq.js

--- luckywheel/5g88/jsimpactsiteautoload4a3b21aq.js
+++ luckywheel/5g88/jsimpactsiteautoload4a3b21aq.js
@@ -1,5 +1,5 @@
 let currentRotationDeg = 0;
-import { db } from "../shared404/limited-pageconfigak41.js"; 
+import { db } from "./shared402/impor34a1220o1si9njs4a20o.js"; 
 import {
   ref, get, set, push, child, runTransaction, onValue, update
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
