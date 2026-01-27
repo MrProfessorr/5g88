7cffa4742bccf40d6d44986c49960b0fc3aeb270
MrProfessorr <mrprofessorr88@gmail.com>
2026-01-27 11:06:58 +0800

    Update cmdconnectpagespmsite888.js

--- luckywheel/superman888/cmdconnectpagespmsite888.js
+++ luckywheel/superman888/cmdconnectpagespmsite888.js
@@ -294,7 +294,7 @@ const colors = ["#FFD166","#F4A261","#E9C46A","#2A9D8F","#264653","#C1121F","#FF
   }
 
 function gotoWheel(code){
-  $("pageEnter")?.classList.add("hidden");
+  $("luckydraw")?.classList.add("hidden");
   $("pageWheel")?.classList.remove("hidden");
   $("codeLabel") && ($("codeLabel").textContent = code);
 }
@@ -303,14 +303,14 @@ function gotoEnter(){
   stopTickLoop();
   $("pageWheel")?.classList.add("hidden");
   $("pageArticle")?.classList.add("hidden");
-  $("pageEnter")?.classList.remove("hidden");
+  $("luckydraw")?.classList.remove("hidden");
   $("promoInput") && ($("promoInput").value = "");
   showEnter("");
 }
 
 function gotoArticle(){
   stopTickLoop();
-  $("pageEnter")?.classList.add("hidden");
+  $("luckydraw")?.classList.add("hidden");
   $("pageWheel")?.classList.add("hidden");
   $("pageArticle")?.classList.remove("hidden");
 }
@@ -600,17 +600,17 @@ setTimeout(async ()=>{
 const btnBackHome = $("btnBackHome");
 if(btnBackHome){
   btnBackHome.onclick = ()=>{
-    location.hash = "#pageEnter";
+    location.hash = "#luckydraw";
     gotoEnter();
   };
 }
 function handleHashPage(){
-  const h = (location.hash || "#pageEnter").toLowerCase();
+  const h = (location.hash || "#luckydraw").toLowerCase();
 
   if(h === "#pagearticle"){
     gotoArticle();
   } else if(h === "#pagewheel"){
-    $("pageEnter")?.classList.add("hidden");
+    $("luckydraw")?.classList.add("hidden");
     $("pageArticle")?.classList.add("hidden");
     $("pageWheel")?.classList.remove("hidden");
   } else {
